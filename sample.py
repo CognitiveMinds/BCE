7eb0e2639b33f9ee4054cc4f3d0fcda44060e023
unknown <bipin.behera@gmail.com>
2020-12-20 19:50:23 +0530

    modifiedsample.py

---
+++ sample.py
@@ -0,0 +1,10 @@
+def bigTest(x,y):
+    if x > y :
+        print( x, " is bigger ")
+    print(y,": is bigger ")
+
+def addTest(x,y):
+    return x + y
+bigTest(205,506)
+value = addTest(405,387)
+print( 'Sum is :',value)
